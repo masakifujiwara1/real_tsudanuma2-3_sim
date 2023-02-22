f4dd6abe627835ff9703f2f066232c62691fc89a
MasakiFujiwara <fmasaki2020@gmail.com>
2023-02-22 18:57:23 +0900

    update

--- experiments/experiment_cit3f_with_gamma.sh
+++ experiments/experiment_cit3f_with_gamma.sh
@@ -1,5 +1,5 @@
 for i in `seq 1`
 do
-  roslaunch real_tsudanuma2-3_sim nav_cloning_sim.launch script:=nav_cloning_with_direction_node.py mode:=selected_training world_name:=tsudanuma_scan.world map_file:=real_tsudanuma2-3.yaml waypoints_file:=real_tsudanuma2-3_way.yaml dist_err:=0.8 initial_pose_x:=-5.0 initial_pose_y:=7.7 initial_pose_a:=3.14 use_waypoint_nav:=true robot_x:=20.38 robot_y:=0.76 robot_Y:=-1.57
+  roslaunch real_tsudanuma2-3_sim nav_cloning_sim.launch script:=nav_cloning_with_direction_node.py mode:=selected_training world_name:=tsudanuma2-3_v2.2.6.world map_file:=real_tsudanuma2-3.yaml waypoints_file:=real_tsudanuma2-3_way.yaml dist_err:=0.8 initial_pose_x:=-5.0 initial_pose_y:=7.7 initial_pose_a:=3.14 use_waypoint_nav:=true robot_x:=0.0 robot_y:=0.0 robot_Y:=0.0
   sleep 10
 done
