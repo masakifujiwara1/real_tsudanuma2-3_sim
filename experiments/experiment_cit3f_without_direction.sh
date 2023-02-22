583589ea1bfb25bb9db7646ec1df21a4f96659a0
MasakiFujiwara <fmasaki2020@gmail.com>
2023-02-22 17:41:18 +0900

    update tsudanuma2-3_v2.2.3

---
+++ experiments/experiment_cit3f_without_direction.sh
@@ -0,0 +1,5 @@
+for i in `seq 1`
+do
+  roslaunch real_tsudanuma2-3_sim nav_cloning_sim.launch script:=nav_cloning_node.py mode:=use_dl_output world_name:=tsudanuma_scan.world map_file:=real_tsudanuma2-3.yaml waypoints_file:=cit_3f_without_direction_way.yaml dist_err:=1.2 initial_pose_x:=-9.78 initial_pose_y:=29.78 initial_pose_a:=-1.57 use_waypoint_nav:=true robot_x:=-1.6 robot_y:=-3.9 robot_Y:=0.0 use_update_amcl_param:=true
+  sleep 20
+done
